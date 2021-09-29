d1665844ebeaeae96fe535c0b521a078f97c385e
Elliot <elliot.widell@elev.ga.ntig.se>
2021-09-29 14:59:38 +0200

    Last Commit

--- FightingSim2/FightingSim2/Program.cs
+++ FightingSim2/FightingSim2/Program.cs
@@ -8,6 +8,8 @@ namespace FightingSim2
         static void Main(string[] args)
         {
 
+            int roundNumb = 1;
+
 
 
 
@@ -15,21 +17,63 @@ namespace FightingSim2
 
             Hero h1 = new Hero();
             Hero h2 = new Hero();
-            Weapon w1 = new Weapon();
-            Weapon w2 = new Weapon();
 
 
-            Console.WriteLine(h1.name);
-            Console.WriteLine(h2.name);
-            Console.WriteLine(w2.weaponName);
-            Console.WriteLine(w2.weaponName);
+
+            Console.WriteLine($"Fighter 1: {h1.name} HP: {h1.hp}");
+            Console.WriteLine($"Fighter 2: {h2.name} HP: {h2.hp}");
+            Console.WriteLine("Press ENTER to see the weapons");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+            Console.WriteLine($"{h1.name} Weapon: {h1.weapon.weaponName} Dmg: {h1.weapon.weaponDmg}");
+            Console.WriteLine($"{h2.name} Weapon: {h2.weapon.weaponName} Dmg: {h2.weapon.weaponDmg}");
+            Console.WriteLine("Press ENTER to start the figth");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+
+
+            while (h1.hp > 0 && h2.hp > 0)
+            {
+
+                Console.WriteLine($"Round: {roundNumb}");
+
+                h1.Attack(h2);
+                h2.Attack(h1);
+                Console.WriteLine($"{h1.name} HP: {h1.hp}");
+                Console.WriteLine($"{h2.name} HP: {h2.hp}");
+                Console.ReadLine();
+                Console.Clear();
+
+                roundNumb++;
+            }
+
+            if (h1.hp > h2.hp)
+            {
+                Console.WriteLine($"{h1.name} Winns!!!");
+            }
+            else
+            {
+
+
+                Console.WriteLine($"{h2.name} Winns!!!");
+            }
+
+
             Console.ReadLine();
 
 
-
-
-
         }
+
+
+
+
+
 
 
     }
--- FightingSim2/FightingSim2/hero.cs
+++ FightingSim2/FightingSim2/hero.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 
 namespace FightingSim2
 {
+
     public class Hero
     {
-        public int hp = 100;
-        public List<string> heroNames = new List<string>(){
+        public int hp = generator.Next(70, 130);
+        public static List<string> heroNames = new List<string>(){
             "Elliot", "Aironas", "Arda", "Rambo", "Osama Binladen"
         };
 
         public string name;
 
-        private Random generator = new Random();
+        private static Random generator = new Random();
+
+        public Weapon weapon = new Weapon();
 
         public Hero()
         {
             int heroNameNumber1 = generator.Next(heroNames.Count);
             name = heroNames[heroNameNumber1];
 
+        }
+
+        public void Attack(Hero target)
+        {
+            target.hp -= weapon.weaponDmg;
         }
 
     }
